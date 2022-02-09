8491c9f4f94197a2bbf38927316162a45f9ad570
ferocide001 <fabian.cook@web.de>
2022-02-09 22:38:29 +0100

    commit

--- Bauernhof.py
+++ Bauernhof.py
@@ -1,20 +1,55 @@
-
-
 class Bauernhof:
-    def __init__(self, bName, bOwner):
+    def __init__(self, bName):
         self.bName = bName
-        self.bOwner = bOwner
-
-    def changeOwner(self, bOwner_new):
-       self.bOwner = bOwner_new 
-
-    def changeName(self, bName_new):
-        self.bName = bName_new
+        self.animals = []
 
     def displayData(self):
-        return f'Bauernhof Name: {self.bName} \nBesitzer Name: {self.bOwner}'
+        return f'Bauernhof Name: {self.bName}'
+
+    def addAnimal(self, animal):
+        self.animals.append(animal)
 
 
 class Tier(Bauernhof):
-    def __init__(self, bName, bOwner, tID, tName, tFutter):
-        super().__init__(bName, bOwner)
+    def __init__(self, bName, tName, tAge):
+        super().__init__(bName)
+        self.tName = tName
+        self.tAge = tAge
+
+    def displayData(self):
+        return f'Mein Name ist {self.tName} und ich bin {self.tAge} Jahr/Jahre alt!'
+
+
+class Kuh(Tier):
+    def __init__(self, bName, tName, tAge):
+        super().__init__(bName, tName, tAge)
+        self.tArt = 'Kuh'
+
+    def speak(self):
+        print('Muu!')
+
+    def __str__(self):
+        return f'Kuh([Bauernhof_Instanz], {self.tName}, {self.tAge})'
+
+
+class Schwein(Tier):
+    def __init__(self, bName, tName, tAge):
+        super().__init__(bName, tName, tAge)
+        self.tArt = 'Schwein'
+
+    def speak(self):
+        print('grunz!')
+
+
+
+bauernhof = Bauernhof('Infofarms')
+
+obj_list = []
+for obj in range(10):
+    obj = Kuh(bauernhof, obj, '20')
+    bauernhof.addAnimal(obj)
+    obj_list.append(obj)
+
+
+for obj in obj_list:
+    print(obj.__str__())
